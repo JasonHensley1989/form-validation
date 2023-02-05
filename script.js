2dbb7a2b0d263f37008ad5456199354cfea003bc
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-05 16:02:43 -0600

    validation basics finished

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ form.addEventListener("submit", (e) => {
     if (name.value === "" || name.value == null) {
         messages.push("A name is required")
     }
+    if (pass.value.length <= 3) {
+        messages.push("Password  must contain at least 4 characters")
+    }
+
+    // This example would be where REGEX would be needed
+
+    if (pass.value !== '!') {
+        messages.push("Your password must contain at least one exclamation mark")
+    }
     
     if(messages.length > 0) {
         e.preventDefault();

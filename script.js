353471d9543e9f720b23b3d7ebeb940e33cac55f
Jason Hensley <jasonhensley1989@gmail.com>
2023-02-05 15:54:57 -0600

    basic layout, event listeners, variables declared

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const name = document.getElementById("name");
 const pass = document.getElementById("password");
 const form = document.getElementById("validTest");
+const errorElement = document.getElementById("error");
 
-console.log(name, pass, form)
+
+form.addEventListener("submit", (e) => {
+    let messages = [];
+    if (name.value === "" || name.value == null) {
+        messages.push("A name is required")
+    }
+    
+    if(messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ')
+    }
+   
+})
